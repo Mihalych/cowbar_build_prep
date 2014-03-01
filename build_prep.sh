ef0da2576a3ee3e9e20274e85316c879c7c13d34
Aleksey Shcherba <ashcherba@mirantis.com>
2014-03-01 15:58:40 +0200

    Add deb package for Ruby libxml

--- build_prep.sh
+++ build_prep.sh
@@ -2,7 +2,7 @@
 
 : ${APT_UPDATE:="false"}
 
-pkgs="debootstrap binutils bridge-utils qemu-utils genisoimage ruby1.8 ruby1.8-dev rubygems1.8 kvm rpm rpm2cpio python-pip gcc make git-core curl"
+pkgs="debootstrap binutils bridge-utils qemu-utils genisoimage ruby1.8 ruby1.8-dev rubygems1.8 kvm rpm rpm2cpio python-pip gcc make git-core curl ruby-libxml"
 gems="kwalify json net-http-digest_auth"
 pips="pip2pi"
 
@@ -11,12 +11,23 @@ APTGET=$(/usr/bin/env which apt-get)
 ${APTGET} install -y --no-install-recommends --no-install-suggests ${pkgs}
 
 PIP=$(/usr/bin/env which pip)
-if [[ -n ${PIP} ]]; then for p in ${pips}; do
-  ${PIP} install ${p}
-done; else echo "pip not found!"; exit 1; fi
+if [[ -n ${PIP} ]];
+  then
+    for p in ${pips}; do
+      ${PIP} install ${p}
+    done;
+  else
+    echo "pip not found!";
+    exit 1;
+fi
 
 GEM=$(/usr/bin/env which gem)
-if [[ -n ${GEM} ]]; then for g in ${gems}; do
-  ${GEM} install ${g}
-done; else echo "gem not found!"; exit 1; fi
-
+if [[ -n ${GEM} ]];
+  then
+    for g in ${gems}; do
+      ${GEM} install ${g}
+    done;
+  else
+    echo "gem not found!";
+    exit 1;
+fi
